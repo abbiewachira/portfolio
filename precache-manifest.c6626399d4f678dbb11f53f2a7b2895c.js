41583226bf8af6e5ac0f6f09d46e2e84ec45e11a
Abbie <wachiragaille2@gmail.com>
2020-01-27 09:35:47 +0300

    Updates

---
+++ precache-manifest.c6626399d4f678dbb11f53f2a7b2895c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761b223476522c9c8a945ee56136dc41",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f0823df8b6cf18b014b0",
+    "url": "/portfolio/static/css/main.11a5f543.chunk.css"
+  },
+  {
+    "revision": "e679fa67b5aacf14b0f7",
+    "url": "/portfolio/static/js/2.65a46ba9.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/portfolio/static/js/2.65a46ba9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0823df8b6cf18b014b0",
+    "url": "/portfolio/static/js/main.d4af9315.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
